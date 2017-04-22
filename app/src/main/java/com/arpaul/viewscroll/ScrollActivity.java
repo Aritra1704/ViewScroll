a1342128bb8be018a7432037956191505d1eaed5
Aritra1704 <aritra1704@gmail.com>
2017-04-22 11:54:13 +0530

    Initial Commit.

---
+++ app/src/main/java/com/arpaul/viewscroll/ScrollActivity.java
@@ -0,0 +1,18 @@
+package com.arpaul.viewscroll;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ARPaul on 22-04-2017.
+ */
+
+public class ScrollActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll);
+    }
+}
